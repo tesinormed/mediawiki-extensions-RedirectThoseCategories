fbd6c1f70cacdd297e1c858a9eb4f3a55ed96fe5
tesinormed <tesinormed@5505.industries>
2025-04-24 10:29:55 -0700

    version 0.1.0

---
+++ src/Hooks.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MediaWiki\Extension\RedirectThoseCategories;
+
+use MediaWiki\Hook\ParserPreSaveTransformCompleteHook;
+use MediaWiki\Page\PageLookup;
+use MediaWiki\Page\RedirectLookup;
+use MediaWiki\Title\Title;
+
+class Hooks implements ParserPreSaveTransformCompleteHook {
+	private const CATEGORY_REGEX = '/\[\[ *[Cc]ategory: *(.+?)(?: *| *\| *(.+?))]]/m';
+
+	private PageLookup $pageLookup;
+	private RedirectLookup $redirectLookup;
+
+	public function __construct( PageLookup $pageLookup, RedirectLookup $redirectLookup ) {
+		$this->pageLookup = $pageLookup;
+		$this->redirectLookup = $redirectLookup;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ParserPreSaveTransformCompleteHook
+	 */
+	public function onParserPreSaveTransformComplete( $parser, &$text ): void {
+		if ( !Title::newFromPageReference( $parser->getPage() )->isContentPage() ) {
+			return;
+		}
+
+		$matchCount = preg_match_all( self::CATEGORY_REGEX, $text, $matches, PREG_SET_ORDER );
+		if ( $matchCount === false || $matchCount === 0 ) {
+			return;
+		}
+		foreach ( $matches as $match ) {
+			// $match[0] is the link itself: [[Category:A category]]
+			// $match[1] is the category name: A category
+
+			$categoryPage = $this->pageLookup->getPageByText( $match[1], defaultNamespace: NS_CATEGORY );
+			if ( $categoryPage === null ) {
+				continue;
+			}
+
+			$categoryPageRedirectTarget = $this->redirectLookup->getRedirectTarget( $categoryPage );
+			if ( $categoryPageRedirectTarget === null || $categoryPageRedirectTarget->getNamespace() !== NS_CATEGORY ) {
+				continue;
+			}
+
+			$text = str_replace( $match[0], '[[Category:' . $categoryPageRedirectTarget->getText() . ']]', $text );
+		}
+	}
+}
